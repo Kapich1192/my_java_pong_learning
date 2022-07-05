e9d8784abe5585abc8cc06216a0b443d0ca7e843
kapich1192 <kapich1192@yandex.ru>
2022-07-05 10:58:46 +0300

    developing

--- src/main/java/application/Pong.java
+++ src/main/java/application/Pong.java
@@ -1,7 +1,9 @@
 package application;
 
+import game_field.GameField;
+
 public class Pong {
     public static void main(String[] args) {
-
+        GameField gameField = new GameField(25,80);
     }
 }
--- src/main/java/game_field/GameField.java
+++ src/main/java/game_field/GameField.java
@@ -1,4 +1,28 @@
 package game_field;
 
 public class GameField {
+    /*fields*/
+    private int heigth;
+    private int weigth;
+    private char[][] gameField;
+    private char fields = 'o';
+    private char line = '|';
+    /*constructors*/
+    public GameField() {}
+    public GameField(int heigth, int weigth) {
+        this.heigth = heigth;
+        this.weigth = weigth;
+        gameField = new char[heigth][weigth];
+    }
+    /*getters*/
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+    /*setters*/
+    /*system*/
+    /*methods*/
 }
