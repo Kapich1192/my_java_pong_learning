f9ea97cc7cbbc8993d89961a903776a94b24d848
kapich1192 <kapich1192@yandex.ru>
2022-07-08 15:19:19 +0300

    developing move boll

--- src/main/java/game_elements/Boll.java
+++ src/main/java/game_elements/Boll.java
@@ -35,6 +35,23 @@ public class Boll {
     /*setters*/
     /*system*/
     public void moveBoll(int heigth, int weigth, Rocket rocket1, Rocket rocket2) {
+        if (bollY + 2 == heigth || bollY - 1 == 0){
+            bollVectorY *= -1;
+        }
+        if ((bollX - 1 == rocket1.getRocketX() && bollY == rocket1.getRocketY()) ||
+                (bollX - 1 == rocket1.getRocketX() && bollY == rocket1.getRocketY() + 1) ||
+                (bollX - 1 == rocket1.getRocketX() && bollY == rocket1.getRocketY() - 1) ||
+                (bollX + 1 == rocket2.getRocketX() && bollY == rocket2.getRocketY()) ||
+                (bollX + 1 == rocket2.getRocketX() && bollY == rocket2.getRocketY() + 1) ||
+                (bollX + 1 == rocket2.getRocketX() && bollY == rocket2.getRocketY() - 1)) {
+            bollVectorX *= -1;
+        }
+        if (bollX > rocket2.getRocketX() || bollX < rocket1.getRocketX()) {
+            bollX = weigth / 2;
+            bollY = heigth / 2;
+        }
+        bollX = bollX + bollVectorX;
+        bollY = bollY + bollVectorY;
 
     }
     /*methods*/
